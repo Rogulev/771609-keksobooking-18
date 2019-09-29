1a1992aa34bce1fd7d533b765950f5070e81113f
Rogulev <Rogulev92@rambler.ru>
2019-09-29 23:42:26 +0500

    Добавил вычисление координат метки и заполенние адреса

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var ENTER_KEYCODE = 13;
 var OFFERS_AMMOUNT = 8;
@@ -6,21 +6,29 @@ var MIN_X = 0;
 var MAX_X = 1200;
 var MIN_Y = 130;
 var MAX_Y = 630;
-var mapPins = document.querySelector('.map__pins');
-var TYPE_HOUSING = ['palace', 'flat', 'house', 'bungalo'];
-var times = ['12:00', '13:00', '14:00'];
-var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var map = document.querySelector('.map');
-var mainPin = document.querySelector('.map__pin--main');
-
+var mapPins = document.querySelector(".map__pins");
+var mainMapPin = document.querySelector(".map__pin--main");
+var TYPE_HOUSING = ["palace", "flat", "house", "bungalo"];
+var times = ["12:00", "13:00", "14:00"];
+var features = [
+  "wifi",
+  "dishwasher",
+  "parking",
+  "washer",
+  "elevator",
+  "conditioner"
+];
+var inputAddress = document.getElementById('address');
+var map = document.querySelector(".map");
+var mainPin = document.querySelector(".map__pin--main");
 
 // Функция получения рандомного числа
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
 // Функция генерации доп.фцнкций жилья
-var getFeatures = function () {
+var getFeatures = function() {
   var newFeatures = [];
   var count = getRandomInt(0, features.length - 1);
   for (var i = 0; i < count; i++) {
@@ -34,7 +42,8 @@ var getPhotos = function(min, max) {
   var photos = [];
   var count = getRandomInt(min, max);
   for (var i = 0; i < count; i++) {
-    photos[i] = 'http://o0.github.io/assets/images/tokyo/hotel' + (i + 1) + '.jpg'
+    photos[i] =
+      "http://o0.github.io/assets/images/tokyo/hotel" + (i + 1) + ".jpg";
   }
   return photos;
 };
@@ -42,16 +51,16 @@ var getPhotos = function(min, max) {
 // Наполнение массива букинг объектами
 var bookingInfo = [];
 
-var createArrayInfo = function () {
+var createArrayInfo = function() {
   for (var i = 0; i < OFFERS_AMMOUNT; i++) {
     bookingInfo[i] = {
       autor: {
-        avatar: 'img/avatars/user0' + (i + 1) + '.png'
+        avatar: "img/avatars/user0" + (i + 1) + ".png"
       },
 
       offer: {
-        title: 'Заголовок Предложения',
-        address: '600, 350',
+        title: "Заголовок Предложения",
+        address: "600, 350",
         price: 1000,
         type: TYPE_HOUSING[getRandomInt(0, TYPE_HOUSING.length - 1)],
         rooms: getRandomInt(1, 3),
@@ -59,29 +68,31 @@ var createArrayInfo = function () {
         checkin: times[getRandomInt(0, 3)],
         checkout: times[getRandomInt(0, 3)],
         features: getFeatures(),
-        description: 'Тут будет описание',
-        photos: getPhotos(1,3)
+        description: "Тут будет описание",
+        photos: getPhotos(1, 3)
       },
 
-      location:{
+      location: {
         x: getRandomInt(MIN_X, MAX_X),
         y: getRandomInt(MIN_Y, MAX_Y)
       }
     };
-    }
+  }
 };
 createArrayInfo();
 
 // Отрисовка pin на карте
-var renderPins = function (info) {
-  var template = document.querySelector('#pin').content.querySelector('.map__pin');
+var renderPins = function(info) {
+  var template = document
+    .querySelector("#pin")
+    .content.querySelector(".map__pin");
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < bookingInfo.length; i++) {
     var element = template.cloneNode(true);
-    var img = element.querySelector('img');
-    element.style.left = info[i].location.x + 'px';
-    element.style.top = info[i].location.y + 'px';
+    var img = element.querySelector("img");
+    element.style.left = info[i].location.x + "px";
+    element.style.top = info[i].location.y + "px";
     img.src = info[i].autor.avatar;
     img.alt = info[i].offer.type;
     fragment.appendChild(element);
@@ -89,46 +100,55 @@ var renderPins = function (info) {
   mapPins.appendChild(fragment);
 };
 
+//Вычисление координат стартовой метки
+var coordinatePinStart = {
+  x: Math.round((mapPins.offsetWidth / 2) + (mainMapPin.offsetWidth / 2)),
+  y: Math.round((mapPins.offsetHeight / 2) + (mainMapPin.offsetHeight / 2))
+}
+
+//Заполнение поля адреса
+inputAddress.value = coordinatePinStart.x + ',' + coordinatePinStart.y;
+
 // Добавление disabled на Формы
-var adForm = document.querySelector('.ad-form');
-var mapFilters = document.querySelector('.map__filters');
+var adForm = document.querySelector(".ad-form");
+var mapFilters = document.querySelector(".map__filters");
 
 var formDisabled = function() {
-  for ( var i = 0; i < adForm.children.length; i++) {
-    adForm.children[i].setAttribute("disabled", true)
+  for (var i = 0; i < adForm.children.length; i++) {
+    adForm.children[i].setAttribute("disabled", true);
   }
-  for ( var i = 0; i < mapFilters.children.length; i++) {
-    mapFilters.children[i].setAttribute("disabled", true)
+  for (var i = 0; i < mapFilters.children.length; i++) {
+    mapFilters.children[i].setAttribute("disabled", true);
   }
 };
 formDisabled();
 
 // Добавление активации страницы по клику
-var mainUnblocking = function () {
-  formUnblocking(),
-  renderPins(bookingInfo);
-}
+var mainUnblocking = function() {
+  formUnblocking(), renderPins(bookingInfo);
+};
 
 var onPinEnterPress = function(evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
-    mainUnblocking()}
+    mainUnblocking();
+  }
 };
 
 var formUnblocking = function() {
-  map.classList.remove('map--faded');
-  adForm.classList.remove('ad-form--disabled');
+  map.classList.remove("map--faded");
+  adForm.classList.remove("ad-form--disabled");
 
-  for ( var i = 0; i < adForm.children.length; i++) {
-    adForm.children[i].removeAttribute("disabled", true)
-  };
-  for ( var i = 0; i < mapFilters.children.length; i++) {
-    mapFilters.children[i].removeAttribute("disabled", true)
-  };
-  mainPin.removeEventListener('mousedown', mainUnblocking);
+  for (var i = 0; i < adForm.children.length; i++) {
+    adForm.children[i].removeAttribute("disabled", true);
+  }
+  for (var i = 0; i < mapFilters.children.length; i++) {
+    mapFilters.children[i].removeAttribute("disabled", true);
+  }
+  mainPin.removeEventListener("mousedown", mainUnblocking);
 
-  mainPin.removeEventListener('keydown', onPinEnterPress);
+  mainPin.removeEventListener("keydown", onPinEnterPress);
 };
 
-mainPin.addEventListener('mousedown', mainUnblocking);
+mainPin.addEventListener("mousedown", mainUnblocking);
 
-mainPin.addEventListener('keydown', onPinEnterPress);
+mainPin.addEventListener("keydown", onPinEnterPress);
