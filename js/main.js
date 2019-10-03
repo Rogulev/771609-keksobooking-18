624b9e79968d61ac60d5ee5c66de5223003c3ae1
Rogulev <Rogulev92@rambler.ru>
2019-10-03 22:51:47 +0500

    Создал сопоставление госте и кол-во комнат в форме

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ var features = [
   "elevator",
   "conditioner"
 ];
-var inputAddress = document.getElementById('address');
+var inputAddress = document.getElementById("address");
 var map = document.querySelector(".map");
 var mainPin = document.querySelector(".map__pin--main");
 
@@ -102,12 +102,12 @@ var renderPins = function(info) {
 
 //Вычисление координат стартовой метки
 var coordinatePinStart = {
-  x: Math.round((mapPins.offsetWidth / 2) + (mainMapPin.offsetWidth / 2)),
-  y: Math.round((mapPins.offsetHeight / 2) + (mainMapPin.offsetHeight / 2))
-}
+  x: Math.round(mapPins.offsetWidth / 2 + mainMapPin.offsetWidth / 2),
+  y: Math.round(mapPins.offsetHeight / 2 + mainMapPin.offsetHeight / 2)
+};
 
 //Заполнение поля адреса
-inputAddress.value = coordinatePinStart.x + ',' + coordinatePinStart.y;
+inputAddress.value = coordinatePinStart.x + "," + coordinatePinStart.y;
 
 // Добавление disabled на Формы
 var adForm = document.querySelector(".ad-form");
@@ -152,3 +152,49 @@ var formUnblocking = function() {
 mainPin.addEventListener("mousedown", mainUnblocking);
 
 mainPin.addEventListener("keydown", onPinEnterPress);
+
+// Сценарий сопоставления гостей и кол-во комнат  в форме
+
+var roomNumber = document.querySelector("#room_number");
+var capacity = document.querySelector("#capacity");
+var options = capacity.querySelectorAll("option");
+
+var disabledOptions = function(opt) {
+  for (var i = 0; i < options.length - 1; i++) {
+    options[i].removeAttribute("disabled", "disabled");
+  }
+
+  if (opt >= 0 && opt <= 2) {
+    options[options.length - 1].setAttribute("disabled", "disabled");
+
+    for (var i = 0; i < opt; i++) {
+      options[i].setAttribute("disabled", "disabled");
+    }
+  } else {
+    for (var i = 0; i < options.length - 1; i++) {
+      options[i].setAttribute("disabled", "disabled");
+    }
+    options[options.length - 1].removeAttribute("disabled");
+  }
+};
+
+roomNumber.addEventListener("change", function() {
+  switch (roomNumber.value) {
+    case "1":
+      disabledOptions(2);
+      //capacity.setCustomValidity("Количество гостей не более 1");
+      break;
+    case "2":
+      disabledOptions(1);
+      //capacity.setCustomValidity("Количество гостей не более 2");
+      break;
+    case "3":
+      disabledOptions(0);
+      //capacity.setCustomValidity("Количество гостей не более 3");
+      break;
+    case "100":
+      disabledOptions(3);
+      //capacity.setCustomValidity("Не для гостей");
+      break;
+  }
+});
